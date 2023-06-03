19de5a5de9ecec57d6b8d1d47df344cab14c950d
NgoKiet97 <ntkiet97dev@gmail.com>
2023-06-03 08:47:20 +0700

    API get all category

---
+++ src/main/java/com/cybersoft/cozastore/controller/CategoryController.java
@@ -0,0 +1,25 @@
+package com.cybersoft.cozastore.controller;
+
+import com.cybersoft.cozastore.payload.response.BaseResponse;
+import com.cybersoft.cozastore.service.imp.ICategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/category")
+public class CategoryController {
+
+    @Autowired
+    ICategoryService iCategoryService;
+
+    @GetMapping("")
+    public ResponseEntity<?> getAllCategory(){
+        BaseResponse baseResponse = new BaseResponse();
+        baseResponse.setData(iCategoryService.getAllCategory());
+        return new ResponseEntity<>(baseResponse, HttpStatus.OK);
+    }
+}
