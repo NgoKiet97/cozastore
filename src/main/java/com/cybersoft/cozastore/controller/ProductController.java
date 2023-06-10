55b26bf9446ffd9f4f77edb96cc27b11d5ba9ea7
NgoKiet97 <ntkiet97dev@gmail.com>
2023-06-10 22:53:55 +0700

    feature/api_download_file

--- src/main/java/com/cybersoft/cozastore/controller/ProductController.java
+++ src/main/java/com/cybersoft/cozastore/controller/ProductController.java
@@ -5,6 +5,10 @@ import com.cybersoft.cozastore.payload.response.BaseResponse;
 import com.cybersoft.cozastore.service.imp.ICategoryService;
 import com.cybersoft.cozastore.service.imp.IProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +23,9 @@ import java.nio.file.StandardCopyOption;
 @RequestMapping("/product")
 public class ProductController {
 
+    @Value("${root.file.path}")
+    private String rootPath;
+
     @Autowired
     IProductService iProductService;
 
@@ -29,12 +36,11 @@ public class ProductController {
 
         return new ResponseEntity<>(baseResponse, HttpStatus.OK);
     }
-
     @PostMapping("")
     public ResponseEntity<?> addProduct(@Valid ProductRequest productRequest){
         String fileName = productRequest.getFileImage().getOriginalFilename();
         try {
-            String rootFolder = "D:\\CyberSoft-20230506T043821Z-001\\CyberSoft\\Spring\\10-05-2023-06-34-44-cozastore-3\\image";
+            String rootFolder = rootPath;
             Path pathRoot = Paths.get(rootFolder);
             if(!Files.exists(pathRoot)){
                 Files.createDirectory(pathRoot);
@@ -46,5 +52,25 @@ public class ProductController {
         }
 
         return new ResponseEntity<>(fileName, HttpStatus.OK);
+    }
+
+    @GetMapping("/file/{filename}")
+    public ResponseEntity<?> downloadFileProduct(@PathVariable String filename){
+        try {
+            Path path = Paths.get(rootPath);
+            Path pathFile = path.resolve(filename);
+            Resource resource = new UrlResource(pathFile.toUri());
+
+            if(resource.exists() || resource.isReadable()){
+                return ResponseEntity.ok()
+                        .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + filename + "\"")
+                        .body(resource);
+            } else{
+                throw new RuntimeException("Không tìm thấy file");
+            }
+        } catch (Exception e){
+            throw new RuntimeException("Không tìm thấy file");
+        }
+
     }
 }
