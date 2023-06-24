0b9d48ba0d205b3bcbb7c3a40c4cfca18914f424
NgoKiet97 <ntkiet97dev@gmail.com>
2023-06-24 10:43:27 +0700

    done full 2nd api_get_all_size

--- src/main/java/com/cybersoft/cozastore/controller/SizeController.java
+++ src/main/java/com/cybersoft/cozastore/controller/SizeController.java
@@ -1,6 +1,8 @@
 package com.cybersoft.cozastore.controller;
 
 import com.cybersoft.cozastore.payload.response.BaseResponse;
+import com.cybersoft.cozastore.service.imp.ISizeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,10 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/size")
 public class SizeController {
 
+    @Autowired
+    private ISizeService iSizeService;
+
     @GetMapping("")
     public ResponseEntity<?> getAllSize(){
         BaseResponse baseResponse = new BaseResponse();
-
+        baseResponse.setData(iSizeService.getAllSize());
         return new ResponseEntity<>(baseResponse, HttpStatus.OK);
     }
 }
