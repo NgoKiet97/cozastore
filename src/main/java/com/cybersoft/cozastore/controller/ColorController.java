01c743f04e2f46d21c1e44958f02df33036934ed
NgoKiet97 <ntkiet97dev@gmail.com>
2023-06-24 15:13:54 +0700

    feature/logging_api_color

--- src/main/java/com/cybersoft/cozastore/controller/ColorController.java
+++ src/main/java/com/cybersoft/cozastore/controller/ColorController.java
@@ -2,6 +2,9 @@ package com.cybersoft.cozastore.controller;
 
 import com.cybersoft.cozastore.payload.response.BaseResponse;
 import com.cybersoft.cozastore.service.imp.IColorService;
+import com.google.gson.Gson;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,12 +16,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/color")
 public class ColorController {
 
+    private Gson gson = new Gson();
+    private Logger logger = LoggerFactory.getLogger(ColorController.class);
+
     @Autowired
     private IColorService iColorService;
     @GetMapping("")
     public ResponseEntity<?> getAllColor(){
         BaseResponse baseResponse = new BaseResponse();
         baseResponse.setData(iColorService.getAllColor());
+
+        String dataLog = gson.toJson(baseResponse);
+        logger.info(dataLog);
 
         return new ResponseEntity<>(baseResponse, HttpStatus.OK);
     }
