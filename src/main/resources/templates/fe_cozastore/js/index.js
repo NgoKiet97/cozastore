ae5735a1fa37b4797a65a4726d317d9615752ec9
NgoKiet97 <ntkiet97dev@gmail.com>
2023-07-16 20:42:48 +0700

    update FE load product

--- src/main/resources/templates/fe_cozastore/js/index.js
+++ src/main/resources/templates/fe_cozastore/js/index.js
@@ -22,8 +22,105 @@ $(document).ready(function () {
             console.log("test", result)
         });
 
+    // load page for the first time
+        $.ajax({
+            method: "GET",
+            url: `http://localhost:8080/product/all`,
+        })
+            .done(function (result) {
+                if (result.data) {
+                    $(".product-by-categoryID-result").remove()
+
+                    $.each(result.data, function (index, value) {
+                        //String literal
+                        var content =
+                            `<div class="col-sm-6 col-md-4 col-lg-3 p-b-35 product-by-categoryID-result">
+                                    <div class="block2">
+                                        <div class="block2-pic hov-img0">
+                                            <img src="${value.image}" alt="IMG-PRODUCT">
+                                            <a href="#"
+                                                class="block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 p-lr-15 trans-04 js-show-modal1">
+                                                Quick View
+                                            </a>
+                                        </div>
+                                        <div class="block2-txt flex-w flex-t p-t-14">
+                                            <div class="block2-txt-child1 flex-col-l ">
+                                                <a href="product-detail.html" class="stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6">
+                                                    ${value.name}
+                                                </a>
+                                                <span class="stext-105 cl3">
+                                                    $${value.price}
+                                                </span>
+                                            </div>
+                                            <div class="block2-txt-child2 flex-r p-t-3">
+                                                <a href="#" class="btn-addwish-b2 dis-block pos-relative js-addwish-b2">
+                                                    <img class="icon-heart1 dis-block trans-04" src="images/icons/icon-heart-01.png" alt="ICON">
+                                                    <img class="icon-heart2 dis-block trans-04 ab-t-l" src="images/icons/icon-heart-02.png" alt="ICON">
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>`
+                        $("#product-by-categoryID").append(content)
+                    })
+                }
+
+                console.log("test", result)
+            });
+    
+        // Click on button "All product"    
+        $("#tab-category").on('click', '#tab-all', function () {
+
+        $.ajax({
+            method: "GET",
+            url: `http://localhost:8080/product/all`,
+        })
+            .done(function (result) {
+                if (result.data) {
+                    $(".product-by-categoryID-result").remove()
+
+                    $.each(result.data, function (index, value) {
+                        //String literal
+                        var content =
+                            `<div class="col-sm-6 col-md-4 col-lg-3 p-b-35 product-by-categoryID-result">
+                                    <div class="block2">
+                                        <div class="block2-pic hov-img0">
+                                            <img src="${value.image}" alt="IMG-PRODUCT">
+                                            <a href="#"
+                                                class="block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 p-lr-15 trans-04 js-show-modal1">
+                                                Quick View
+                                            </a>
+                                        </div>
+                                        <div class="block2-txt flex-w flex-t p-t-14">
+                                            <div class="block2-txt-child1 flex-col-l ">
+                                                <a href="product-detail.html" class="stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6">
+                                                    ${value.name}
+                                                </a>
+                                                <span class="stext-105 cl3">
+                                                    $${value.price}
+                                                </span>
+                                            </div>
+                                            <div class="block2-txt-child2 flex-r p-t-3">
+                                                <a href="#" class="btn-addwish-b2 dis-block pos-relative js-addwish-b2">
+                                                    <img class="icon-heart1 dis-block trans-04" src="images/icons/icon-heart-01.png" alt="ICON">
+                                                    <img class="icon-heart2 dis-block trans-04 ab-t-l" src="images/icons/icon-heart-02.png" alt="ICON">
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>`
+                        $("#product-by-categoryID").append(content)
+                    })
+                }
+
+                console.log("test", result)
+            });
+
+    })
+
+    // Click on button "Product by category"    
     $("#tab-category").on('click', '.btn-category', function () {
-        
+
         var id = $(this).attr('custom-id')
 
         $.ajax({
@@ -32,12 +129,12 @@ $(document).ready(function () {
         })
             .done(function (result) {
                 if (result.data) {
-                    // $("#product-by-categoryID").remove()
+                    $(".product-by-categoryID-result").remove()
 
                     $.each(result.data, function (index, value) {
                         //String literal
                         var content =
-                            `<div class="col-sm-6 col-md-4 col-lg-3 p-b-35 women">
+                            `<div class="col-sm-6 col-md-4 col-lg-3 p-b-35 product-by-categoryID-result">
                                 <div class="block2">
                                     <div class="block2-pic hov-img0">
                                         <img src="${value.image}" alt="IMG-PRODUCT">
