605369d1a7c266478e776d5ea5b923abab62d375
NgoKiet97 <ntkiet97dev@gmail.com>
2023-07-16 21:28:20 +0700

    update homepage  button load more product

--- src/main/resources/templates/fe_cozastore/js/index.js
+++ src/main/resources/templates/fe_cozastore/js/index.js
@@ -22,7 +22,65 @@ $(document).ready(function () {
             console.log("test", result)
         });
 
-    // load page for the first time
+    // load all product default
+    $.ajax({
+        method: "GET",
+        url: `http://localhost:8080/product/all`,
+    })
+        .done(function (result) {
+            if (result.data) {
+                $(".product-by-categoryID-result").remove()
+
+                $.each(result.data, function (index, value) {
+                    //String literal
+                    var content =
+                        `<div class="col-sm-6 col-md-4 col-lg-3 p-b-35 product-by-categoryID-result" style="display: none">
+                                    <div class="block2">
+                                        <div class="block2-pic hov-img0">
+                                            <img src="${value.image}" alt="IMG-PRODUCT">
+                                            <a href="#"
+                                                class="block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 p-lr-15 trans-04 js-show-modal1">
+                                                Quick View
+                                            </a>
+                                        </div>
+                                        <div class="block2-txt flex-w flex-t p-t-14">
+                                            <div class="block2-txt-child1 flex-col-l ">
+                                                <a href="product-detail.html" class="stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6">
+                                                    ${value.name}
+                                                </a>
+                                                <span class="stext-105 cl3">
+                                                    $${value.price}
+                                                </span>
+                                            </div>
+                                            <div class="block2-txt-child2 flex-r p-t-3">
+                                                <a href="#" class="btn-addwish-b2 dis-block pos-relative js-addwish-b2">
+                                                    <img class="icon-heart1 dis-block trans-04" src="images/icons/icon-heart-01.png" alt="ICON">
+                                                    <img class="icon-heart2 dis-block trans-04 ab-t-l" src="images/icons/icon-heart-02.png" alt="ICON">
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>`
+                    $("#product-by-categoryID").append(content)
+                })
+            }
+
+            console.log("test", result)
+
+            //show product 
+            $(".product-by-categoryID-result").slice(0, 4).show()
+            $("#btn-load-more").on("click", function (e) {
+                e.preventDefault();
+                $(".product-by-categoryID-result:hidden").slice(0, 4).slideDown();
+                if ($(".product-by-categoryID-result:hidden").length == 0) {
+                    $("#btn-load-more").text("Out of product").addClass("noContent");
+                }
+            })
+        });
+
+    // Click on button "All product"    
+    $("#tab-category").on('click', '#tab-all', function () {
+
         $.ajax({
             method: "GET",
             url: `http://localhost:8080/product/all`,
@@ -34,7 +92,7 @@ $(document).ready(function () {
                     $.each(result.data, function (index, value) {
                         //String literal
                         var content =
-                            `<div class="col-sm-6 col-md-4 col-lg-3 p-b-35 product-by-categoryID-result">
+                            `<div class="col-sm-6 col-md-4 col-lg-3 p-b-35 product-by-categoryID-result" style="display: none">
                                     <div class="block2">
                                         <div class="block2-pic hov-img0">
                                             <img src="${value.image}" alt="IMG-PRODUCT">
@@ -66,54 +124,17 @@ $(document).ready(function () {
                 }
 
                 console.log("test", result)
-            });
-    
-        // Click on button "All product"    
-        $("#tab-category").on('click', '#tab-all', function () {
 
-        $.ajax({
-            method: "GET",
-            url: `http://localhost:8080/product/all`,
-        })
-            .done(function (result) {
-                if (result.data) {
-                    $(".product-by-categoryID-result").remove()
+                $(".product-by-categoryID-result").slice(0, 4).show()
 
-                    $.each(result.data, function (index, value) {
-                        //String literal
-                        var content =
-                            `<div class="col-sm-6 col-md-4 col-lg-3 p-b-35 product-by-categoryID-result">
-                                    <div class="block2">
-                                        <div class="block2-pic hov-img0">
-                                            <img src="${value.image}" alt="IMG-PRODUCT">
-                                            <a href="#"
-                                                class="block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 p-lr-15 trans-04 js-show-modal1">
-                                                Quick View
-                                            </a>
-                                        </div>
-                                        <div class="block2-txt flex-w flex-t p-t-14">
-                                            <div class="block2-txt-child1 flex-col-l ">
-                                                <a href="product-detail.html" class="stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6">
-                                                    ${value.name}
-                                                </a>
-                                                <span class="stext-105 cl3">
-                                                    $${value.price}
-                                                </span>
-                                            </div>
-                                            <div class="block2-txt-child2 flex-r p-t-3">
-                                                <a href="#" class="btn-addwish-b2 dis-block pos-relative js-addwish-b2">
-                                                    <img class="icon-heart1 dis-block trans-04" src="images/icons/icon-heart-01.png" alt="ICON">
-                                                    <img class="icon-heart2 dis-block trans-04 ab-t-l" src="images/icons/icon-heart-02.png" alt="ICON">
-                                                </a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>`
-                        $("#product-by-categoryID").append(content)
-                    })
-                }
-
-                console.log("test", result)
+                $("#btn-load-more").text("Load more").removeClass("noContent")
+                $("#btn-load-more").on("click", function (e) {
+                    e.preventDefault();
+                    $(".product-by-categoryID-result:hidden").slice(0, 4).slideDown();
+                    if ($(".product-by-categoryID-result:hidden").length == 0) {
+                        $("#btn-load-more").text("Out of product").addClass("noContent");
+                    }
+                })
             });
 
     })
@@ -134,7 +155,7 @@ $(document).ready(function () {
                     $.each(result.data, function (index, value) {
                         //String literal
                         var content =
-                            `<div class="col-sm-6 col-md-4 col-lg-3 p-b-35 product-by-categoryID-result">
+                            `<div class="col-sm-6 col-md-4 col-lg-3 p-b-35 product-by-categoryID-result" style="display: none">
                                 <div class="block2">
                                     <div class="block2-pic hov-img0">
                                         <img src="${value.image}" alt="IMG-PRODUCT">
@@ -166,8 +187,21 @@ $(document).ready(function () {
                 }
 
                 console.log("test", result)
+
+                $(".product-by-categoryID-result").slice(0, 4).show()
+
+                $("#btn-load-more").text("Load more").removeClass("noContent")
+                $("#btn-load-more").on("click", function (e) {
+                    e.preventDefault();
+                    $(".product-by-categoryID-result:hidden").slice(0, 4).slideDown();
+                    if ($(".product-by-categoryID-result:hidden").length == 0) {
+                        $("#btn-load-more").text("Out of product").addClass("noContent");
+                    }
+                })
             });
 
     })
+
+    
 
 })
